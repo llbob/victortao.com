ffb9cd2af7e4140bcd61391601224b1ff6253b5e
llbob <miniporse@gmail.com>
2025-03-22 15:44:25 +0100

    Style iphone header content

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#EEE" />
         <link rel="stylesheet" href="/style.css" />
         <link rel="icon" href="/favicon.ico" />
         <title>Victor Tao</title>
